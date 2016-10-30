bb378f204bc7fbf6d13c7d4e77eb8066965344b8
Darrell Wright <dwright-git@dawdevel.ca>
2016-10-30 17:28:28 -0400

    Added ability to enable/disable trend arrows, bg delta, and reading age

--- nightscout_indicator.py
+++ nightscout_indicator.py
@@ -53,6 +53,9 @@ if not os.path.isfile(configfile_name):
     config = configparser.ConfigParser()
     config.add_section('main')
     config.set('main', 'night_scout_url_base', 'https://servername.dom/')
+    config.set('main', 'show_trend', 'True' )
+    config.set('main', 'show_bgdelta', 'True' )
+    config.set('main', 'show_age', 'True' )
     with open(configfile_name, 'w') as f:
         config.write(f)
     print('Please update ' + configfile_name)
@@ -60,16 +63,15 @@ if not os.path.isfile(configfile_name):
 
 
 class Indicator():
-    def __init__(self):
-
+    def __init__( self ):
         self.app = 'Nightscout Indicator'
         iconpath = "nightscout_indicator_icon"
         self.config = configparser.RawConfigParser()
         self.config.read(configfile_name)
-        #self.indicator = AppIndicator3.Indicator.new(self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER)
-        self.indicator = AppIndicator3.Indicator.new_with_path(self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER,  os.path.dirname(os.path.realpath(__file__) ) )
+        #self.indicator = AppIndicator3.Indicator.new( self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER)
+        self.indicator = AppIndicator3.Indicator.new_with_path( self.app, iconpath, AppIndicator3.IndicatorCategory.OTHER,  os.path.dirname(os.path.realpath(__file__) ) )
         self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
-        self.indicator.set_menu(self.create_menu())
+        self.indicator.set_menu( self.create_menu())
         self.indicator.set_label("No Data", self.app)
         # the thread:
         self.update = Thread(target=self.fetch_ns)
@@ -77,7 +79,7 @@ class Indicator():
         self.update.setDaemon(True)
         self.update.start()
 
-    def create_menu(self):
+    def create_menu( self ):
         menu = Gtk.Menu()
         # menu item 1
         # separator
@@ -89,48 +91,63 @@ class Indicator():
         menu.show_all()
         return menu
 
-    def fetch_ns_status(self):
-        glucose = "No Data"
-        url = urljoin(self.config.get('main', 'night_scout_url_base'), '/pebble')
-        r = requests.get( url )
-        if 200 != r.status_code:
-            return "No Data"
+    def calc_age_minutes( self, server_now, sgv_time ):
+        return str( math.floor((int( server_now ) - int( sgv_time ))/60000) )
 
-        arrows = { 0:'', 1:'⇈', 2: '↑', 3:'↗', 4:'→', 5:'↘', 6: '↓', 7: '⇊' }
-        glucose = 'No Data'
-       # try:
-        j = r.json( )
-        bg_info = j['bgs'][0]
-        last_info = math.floor((int( j['status'][0]['now'] ) - int(bg_info['datetime']))/60000)
+    def build_display( self, json_resp ):
+        bg_info = json_resp['bgs'][0]
+
         sgv = bg_info['sgv']
         if float( sgv ) == 0.7:
             result = 'No Signal'
+        elif float( sgv ) == 0.3:
+            result = 'Calibration Needed'
         else:
-            result = bg_info['sgv'] + ' ' + arrows[bg_info['trend']] + ' (' + bg_info['bgdelta'] + ') '
+            result = sgv
+            if self.config.get('main', 'show_trend' ) in [ 'True', 'true', 'yes', 'Yes' ]:
+                arrows = { 0:'', 1:'⇈', 2: '↑', 3:'↗', 4:'→', 5:'↘', 6: '↓', 7: '⇊' }
+                result += ' ' + arrows[bg_info['trend']] 
 
-        result = result + '[' + str(last_info) + ']'
-        glucose = result
+            if self.config.get('main', 'show_bgdelta' ) in [ 'True', 'true', 'yes', 'Yes' ]:
+                result += ' (' + bg_info['bgdelta'] + ')'
+
+            if self.config.get('main', 'show_age' ) in [ 'True', 'true', 'yes', 'Yes' ]:
+                result += ' [' + self.calc_age_minutes( json_resp['status'][0]['now'], bg_info['datetime'] ) + ']'
+
+        return result
+
+    def fetch_ns_status( self ):
+        glucose = "No Data"
+        url = urljoin( self.config.get('main', 'night_scout_url_base'), '/pebble')
+        r = requests.get( url )
+
+        if 200 != r.status_code:
+            return 'No Data'
+
+        result = 'No Data'
+        #try:
+        tmp = self.build_display( r.json( ) )
+        result = tmp
         #except:
         #    pass
+        return result
 
-        return glucose
-
-    def fetch_ns(self):
+    def fetch_ns( self ):
         t = 2
         while True:
             mention = self.fetch_ns_status()
             # apply the interface update using  GObject.idle_add()
-            GObject.idle_add(self.indicator.set_label, mention, self.app, priority=GObject.PRIORITY_DEFAULT)
+            GObject.idle_add( self.indicator.set_label, mention, self.app, priority=GObject.PRIORITY_DEFAULT)
             time.sleep(60)
             t += 1
 
-    def stop(self, source):
+    def stop( self, source):
         Gtk.main_quit()
 
-    def run(self):
+    def run( self ):
         Gtk.main()
 
-    def signal_exit(self, signum, frame):
+    def signal_exit( self, signum, frame):
         print( 'Recieved signal: ', signum )
         print( 'Quitting...' )
         self.stop( )
