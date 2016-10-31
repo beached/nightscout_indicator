af6748e0e784e38ca62a8fd1fd32ae168d11fc0e
Darrell Wright <dwright-git@dawdevel.ca>
2016-10-30 21:33:18 -0400

    Reorganized file and added setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup( name='nightscout_indicator',
+        version='0.1',
+        description='Display pertinent Nightscout data',
+        url='https://github.com/beached/nightscout_indicator',
+        author='Darrell Wright',
+        author_email='Darrell.Wright@gmail.com',
+        license='MIT',
+        packages=['nightscout_indicator'],
+        install_requires=[
+            'requests',
+            'gi',
+            'pysocks'
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3'
+        ],
+        zip_safe=False)
+
+
+
